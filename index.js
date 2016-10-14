e6a1dc9c1bfb5c3ce78734a57f4a9b41966cddeb
lzl <lizunlong@gmail.com>
2016-10-14 20:24:30 +0800

    pages

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
+var path = require('path');
 var bodyParser = require('body-parser');
 var wechat = require('./lib/wechat');
 var config = require('./lib/config');
@@ -7,14 +9,23 @@ app.use(bodyParser.urlencoded({
   extended: false,
 }));
 
+app.set('views', path.join(__dirname, 'lib/views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'static'), {
+  index: false,
+  maxAge: '7 days'
+}));
+
 wechat(config.wechat);
 
 wechat.createMenu(require('./lib/menu.json'));
 
 app.use('/wxapi', require('./lib/routers/wxapi.js'));
+app.use('/pages', require('./lib/routers/pages.js'));
 
 app.get('/', function(req, res, next) {
-  res.send('1474902499322');
+  res.send('lizunlong');
 });
 
 app.listen(8007, function(error) {
